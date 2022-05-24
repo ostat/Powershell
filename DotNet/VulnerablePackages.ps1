ca63e40f82b15b4aae8fb6b18aa1890607bac72a
chris <chris@ostat.com>
2022-05-24 22:58:37 +1000

    added VulnerablePackages script

---
+++ DotNet/VulnerablePackages.ps1
@@ -0,0 +1,50 @@
+﻿<#
+    .SYNOPSIS
+    Script reports projects with know known vulnerable packages
+
+    .PARAMETER SourcePath
+    Root seach folder all solutions in child folder will be reported on.
+
+    .PARAMETER PackageFilter
+    Filter the report of packages
+
+    .INPUTS
+    None. You cannot pipe objects to Add-Extension.
+
+    .EXAMPLE
+    PS> .\VulnerablePackages.ps1 -SourcePath 'c:\src\project'
+#>
+param(
+    [string]$SourcePath = $PSScriptRoot
+)
+
+function CheckForVulnerablePackages
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$SolutionPath
+    )
+
+    Write-Host "Reading solution: '$SolutionPath'" -ForegroundColor Green
+
+    $solutionItem = get-Item $SolutionPath
+    Get-Content $SolutionPath | `
+        where { $_ -match "Project.+, ""(.+.csproj)""," } | `
+        foreach {
+            $ConfigPath = Join-Path $solutionItem.Directory.FullName $matches[1]
+            if(test-path -LiteralPath $ConfigPath)
+            {
+                $projectItem = Get-Item $ConfigPath
+         
+                dotnet list $projectItem.FullName package --vulnerable --include-transitive --source 'https://api.nuget.org/v3/index.json' | Tee-Object -Variable 'output' | Out-Null
+                $output = [system.String]::Join("`r`n", $output)
+                if(!$output.Contains('has no vulnerable packages'))
+                {
+                    Write-Warning "Project has vulnerable packages : '$ConfigPath'"
+                    Write-Host $output
+                }
+            }
+        }
+}
+
+Get-ChildItem -LiteralPath $SourcePath -recurse -Filter *.sln | foreach { CheckForVulnerablePackages $_.FullName }
--- DotNet/readme.md
+++ DotNet/readme.md
@@ -1,6 +1,10 @@
 # DotNet Scripts
 
 ----
+## Vulnerable Packages
+Powershell script to check nuget packages used in project for know vulnerabilities that have been reported to nuget.org.
+
+----
 ## Run Tests
 
 PowerShell script for running all test projects in a given folder or child folder. Once execution is completed a test summary is displayed.
